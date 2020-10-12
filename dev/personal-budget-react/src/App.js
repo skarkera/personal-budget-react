d4a3714640953294499f1d1d66cd6ef1aaff1741
skarkera <sabdakarkera@Sabdas-MacBook-Pro.local>
2020-10-12 17:09:40 -0400

    Adding project files

---
+++ dev/personal-budget-react/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import './App.css';
+import Menu from './Menu/Menu';
+import Hero from './Hero/Hero';
+import HomePage from './HomePage/HomePage';
+import Footer from './Footer/Footer';
+import AboutPage from './AboutPage/AboutPage';
+import LoginPage from './LoginPage/LoginPage';
+
+function App() {
+  return (
+    <Router>
+      <Menu/>
+      <Hero/>
+      <div className="mainContainer">
+        <Switch>
+          <Route path="/about">
+            <AboutPage/>
+          </Route>
+          <Route path="/login">
+            <LoginPage/>
+          </Route>
+          <Route path="/">
+            <HomePage/>
+          </Route>
+        </Switch>
+      </div>
+      <Footer/>
+    </Router>
+  );
+}
+
+export default App;
